4a7b44c998ad85afa4afbf5ad2a039ea0639c32d
atharv03 <atharvchauhan03@gmail.com>
2023-12-01 10:25:39 +0530

    set environment

--- admin/includes/confile.php
+++ admin/includes/confile.php
@@ -1,3 +1,9 @@
-<?php
-$con = mysqli_connect('localhost','sellit','NqPTi#HY9A8wKM','sellit');
+<?php  
+require dirname(__DIR__)."/../env.php";
+// Use the constants in your script
+$host = DB_HOST;
+$username = DB_USERNAME;
+$password = DB_PASSWORD;
+$database = DB_DATABASE; 
+$con = mysqli_connect($host, $username, $password, $database); 
 ?>
