5e302d75b85e0f6f2dab83927a2078402b7815e7
atharv03 <atharvchauhan03@gmail.com>
2023-12-04 19:54:26 +0530

    set server environment variables

--- admin/includes/confile.php
+++ admin/includes/confile.php
@@ -1,9 +1,11 @@
 <?php  
 require dirname(__DIR__)."/../env.php";
 // Use the constants in your script
-$host = DB_HOST;
-$username = DB_USERNAME;
-$password = DB_PASSWORD;
-$database = DB_DATABASE; 
-$con = mysqli_connect($host, $username, $password, $database); 
+
+$dbHost = $_SERVER['DB_HOST'];
+$dbUsername = $_SERVER['DB_USERNAME'];
+$dbPassword = $_SERVER['DB_PASSWORD'];
+$dbDatabase = $_SERVER['DB_DATABASE'];
+
+$con = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbDatabase); 
 ?>
