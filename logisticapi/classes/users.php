244a61be5f0b8e1757d9d44c6e5da03821d0a845
atharv03 <atharvchauhan03@gmail.com>
2024-01-14 19:40:49 +0530

    save as

--- logisticapi/classes/users.php
+++ logisticapi/classes/users.php
@@ -340,7 +340,7 @@ class Users
         );
         $usermobileno = $userrecord["mobile"];
         $name = $userrecord["name"];
-        echo "<<<--->>>" . $accountno;
+        // echo "<<<--->>>" . $accountno;
         // $upiid = $accountdetail['upi'];
         // new code start
         date_default_timezone_set("Asia/Calcutta");
